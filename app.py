8dd5052c6571a4b1b0779201cfd03ef6d6703106
HenriBilsing <152088398+HenriBilsing@users.noreply.github.com>
2023-11-27 22:16:55 +0100

    Input import fix

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 import streamlit as st
-import input_stage  # Importing the input_stage module
+import Input_stage
 
 def main():
     st.title("Business Locator Application")
 
     # Calling the input stage function
-    user_location, business_category = input_stage.input_stage()
+    user_location, business_category = Input_stage.input_stage()
 
     # Display the results from the input stage
     st.write("### Selected Location and Category")
     st.write(f"Location: {user_location}")
     st.write(f"Category: {business_category}")
 
-    # Here you can add further steps like API_stage and Output_stage
+
 
 if __name__ == "__main__":
     main()
