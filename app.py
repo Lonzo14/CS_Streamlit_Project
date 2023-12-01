27b79e21e8ce561d47301ab29267614fd115177c
Lonzo14 <91062425+Lonzo14@users.noreply.github.com>
2023-12-01 11:37:16 +0100

    v3

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import Input_stage
 import Output_stage
+import pandas as pd
+import math
 
 
 def main():
@@ -21,11 +23,18 @@ def main():
     radius_km = st.number_input('Radius in km', value=5)
     business_type = st.selectbox('Business Type', ['Restaurant', 'Cafe', 'Retail'])
 
+# Example usage
+    data = [
+        {'name': 'Restaurant A', 'review': 'Excellent', 'type': 'Restaurant', 'latitude': 40.7128, 'longitude': -74.0060},
+        {'name': 'Cafe B', 'review': 'Very Good', 'type': 'Cafe', 'latitude': 40.7328, 'longitude': -74.0160},
+        {'name': 'Retail C', 'review': 'Good', 'type': 'Retail', 'latitude': 40.7528, 'longitude': -74.0260}
+    ]
+
     # Button to perform action
     if st.button('Find Businesses'):
         user_coordinates = (lat, lon)
         # Assuming 'data' is your dataset
-        results = businesses_in_radius(user_coordinates, radius_km, business_type, data)
+        results = Output_stage.businesses_in_radius(user_coordinates, radius_km, business_type, data)
         
         if results:
             # Prepare DataFrame for st.map
